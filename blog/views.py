cfb46cc5476e6a4daba92380efb20152b8f0d4af
ahmsolt <157969838+ahmsolt@users.noreply.github.com>
2024-10-26 11:25:51 +0330

    01

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,7 @@ def blog_view(request):
     x1 = Post.objects.all()
     x2 = {'x1':x1}
     return render (request,'blog/blog-home.html',x2)
-def blog_single(request):
-    return render (request,'blog/blog-single.html')
+def blog_single(request,pid):
+    y1 = get_object_or_404(Post,pk=pid)
+    y2 = {'y1':y1}
+    return render (request,'blog/blog-single.html',y2)
